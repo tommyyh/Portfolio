4c8a0447c24119f22030858d0f5feca89c48a99d
Tommy Hoang <tommyyhoangg@gmail.com>
2021-08-13 18:03:41 +0200

    Added nav for desktop

--- static/js/all/nav.js
+++ static/js/all/nav.js
@@ -10,3 +10,25 @@
       nav.className === 'burger burger_open' ? 'hidden' : 'initial';
   });
 })();
+
+(() => {
+  const getElemet = (className) => {
+    const element = document.querySelector(className).querySelector('a');
+
+    return element;
+  };
+
+  // Highlight a link that the user is currently on
+  const about = getElemet('#nav__about');
+  const projects = getElemet('#nav__projects');
+  const contact = getElemet('#nav__contact');
+  const pathName = window.location.pathname;
+
+  if (pathName === '/about/') {
+    about.style.color = '#fff';
+  } else if (pathName === '/projects/') {
+    projects.style.color = '#fff';
+  } else if (pathName === '/contact/') {
+    contact.style.color = '#fff';
+  }
+})();
