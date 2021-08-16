db602994da4e04b6b9ff487a4a82ceb91c4350ce
Tommy Hoang <tommyyhoangg@gmail.com>
2021-08-16 18:21:44 +0200

    Deploy try #3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,8 +126,8 @@ MEDIA_URL = '/images/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_POST = 25
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL')
 EMAIL_HOST_PASSWORD = config('PASSWORD')
