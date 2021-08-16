3cb9c78d4abd6f884db9403fb30f86fe3ea38b38
Tommy Hoang <tommyyhoangg@gmail.com>
2021-08-16 13:18:11 +0200

    Deploy try num 2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tommyhoang.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['tommyhoang.herokuapp.com', 'localhost']
 
 
 # Application definition
